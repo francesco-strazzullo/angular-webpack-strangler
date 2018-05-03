99c6b585dfb9fd1e72795e395ee8d98ee44427ee
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-05-03 11:26:35 +0200

    using webcomponents

--- app/es6/index.js
+++ app/es6/index.js
@@ -1,4 +1,9 @@
 import { angularElement as tweets } from './services/tweets'
+import ProgressBar from './components/ProgressBar'
+import CharacterCounter from './components/CharacterCounter'
+
+window.customElements.define('my-app-progress-bar', ProgressBar)
+window.customElements.define('my-app-character-counter', CharacterCounter)
 
 const bindToAngular = (angular, moduleName) => {
   const angularModule = angular.module(moduleName)
